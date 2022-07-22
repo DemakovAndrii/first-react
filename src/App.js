57064b7136b322cd49e70d431250e353e5e692f7
demakov15 <demakov1155@gmail.com>
2022-07-22 22:33:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import './styles/App.css'
 
 function App() {
-
+	const [posts, setPosts] = useState([
+		{ id: 1, title: 'aaa', body: 'bbb' },
+		{ id: 12, title: 'aaa2', body: 'bbb2' },
+		{ id: 123, title: 'aaa23', body: 'bbb23' },
+	])
 
 	return (
 		<div className="App">
-			<Counter />
+			<form>
+				<input type='text' placeholder='Название' />
+				<input type='text' placeholder='Описание' />
+				<button>Создать пост</button>
+			</form>
+			<PostList posts={posts} title='Список постов 1' />
 		</div>
 	);
 }
 
-export default App;
+export default App;  
